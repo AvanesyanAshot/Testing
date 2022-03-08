b1e86b2b36c651d6564c633445483cddd168ceab
AvanesyanAshot <9teron9@gmail.com>
2022-03-08 19:30:08 +0300

    feat: add test to app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,31 @@
+/* eslint-disable testing-library/no-debugging-utils */
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
 
-test("рендер Hello wolrd", () => {
-  render(<App />);
-  const helloWorld = screen.getByText(/hello world/i);
-  const btn = screen.getByRole("button");
-  const input = screen.getByPlaceholderText(/input/i);
+describe("App", () => {
+  test("рендер Hello world", () => {
+    render(<App />);
+    const helloWorld = screen.getByText(/hello world/i);
+    const btn = screen.getByRole("button");
+    const input = screen.getByPlaceholderText(/input/i);
 
-  expect(helloWorld).toBeInTheDocument();
-  expect(helloWorld).toBeInTheDocument();
-  expect(helloWorld).toBeInTheDocument();
+    expect(helloWorld).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+    screen.debug();
+  });
+  test("query input", () => {
+    render(<App />);
+    const btn = screen.queryByText(/test/i);
+    expect(btn).toBeNull();
+  });
+
+  test("find input", async () => {
+    render(<App />);
+    screen.debug();
+    const btn = await screen.findByText(/data/i);
+    expect(btn).toBeInTheDocument();
+    screen.debug();
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null as any);
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 1000);
+  }, []);
+
   return (
     <div>
+      {data && <div>data</div>}
       <h1>Hello world</h1>
       <button>Добавить</button>
-      <input type="text" placeholder="input" />
+      <input type="text" placeholder="input..." />
     </div>
   );
 }
